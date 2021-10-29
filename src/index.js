f473747e10493ea7adcfac37db473b0f43b2f497
Dylan Premo <cyhyraethz@gmail.com>
2021-10-29 11:30:42 -0700

    Add image below navbar for restaurant page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './style.css';
+// import Vegan from './vegan.jpg';
+
+// const content = document.getElementById('content');
+
+// const vegan = new Image();
+// vegan.src = Vegan;
+
+// content.appendChild(vegan);
 
 console.log('This is a test');
