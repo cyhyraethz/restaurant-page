ce06fb9e2391130cdef0ca45a0a519612faa7ae2
Dylan Premo <cyhyraethz@gmail.com>
2021-12-29 17:00:36 -0800

    Add telephone and email links to icons

--- src/contact-tab.js
+++ src/contact-tab.js
@@ -10,20 +10,24 @@ export default function loadContactTab() {
   const contactContainer = document.createElement('div');
 
   const orderPhoneContainer = document.createElement('div');
-  const orderPhoneText = document.createElement('p');
+  const orderPhoneLink = document.createElement('a');
   const orderPhoneIcon = document.createElement('i');
-
-  const officePhoneContainer = document.createElement('div');
-  const officePhoneText = document.createElement('p');
-  const officePhoneIcon = document.createElement('i');
-
-  const arloMailContainer = document.createElement('div');
-  const arloMailText = document.createElement('p');
-  const arloMailIcon = document.createElement('i');
+  const orderPhoneText = document.createElement('p');
 
   const dylanMailContainer = document.createElement('div');
-  const dylanMailText = document.createElement('p');
+  const dylanMailLink = document.createElement('a');
   const dylanMailIcon = document.createElement('i');
+  const dylanMailText = document.createElement('p');
+
+  const arloMailContainer = document.createElement('div');
+  const arloMailLink = document.createElement('a');
+  const arloMailIcon = document.createElement('i');
+  const arloMailText = document.createElement('p');
+
+  const officePhoneContainer = document.createElement('div');
+  const officePhoneLink = document.createElement('a');
+  const officePhoneIcon = document.createElement('i');
+  const officePhoneText = document.createElement('p');
 
   contactName.id = 'contactName';
   contactName.innerText = 'Contact Us';
@@ -32,21 +36,25 @@ export default function loadContactTab() {
   contactContainer.className = 'contactContainer';
 
   orderPhoneContainer.className = 'contactItemContainer';
+  orderPhoneLink.href = 'tel:8315550189';
   orderPhoneIcon.className = 'fas fa-phone-alt';
   orderPhoneIcon.setAttribute('aria-hidden', 'true');
   orderPhoneText.innerText = 'Call to Place an Order: 831.555.0189';
 
   dylanMailContainer.className = 'contactItemContainer';
+  dylanMailLink.href = 'mailto:dylan@thegreensproutrestaurant.com';
   dylanMailIcon.className = 'fas fa-envelope';
   dylanMailIcon.setAttribute('aria-hidden', 'true');
   dylanMailText.innerText = 'dylan@thegreensproutrestaurant.com';
 
   arloMailContainer.className = 'contactItemContainer';
+  arloMailLink.href = 'mailto:arlo@thegreensproutrestaurant.com';
   arloMailIcon.className = 'fas fa-envelope';
   arloMailIcon.setAttribute('aria-hidden', 'true');
   arloMailText.innerText = 'arlo@thegreensproutrestaurant.com';
 
   officePhoneContainer.className = 'contactItemContainer';
+  officePhoneLink.href = 'tel:8315550190';
   officePhoneIcon.className = 'fas fa-phone-alt';
   officePhoneIcon.setAttribute('aria-hidden', 'true');
   officePhoneText.innerText = 'Office Number: 831.555.0190';
@@ -56,18 +64,22 @@ export default function loadContactTab() {
   content.appendChild(contactContainer);
 
   contactContainer.appendChild(orderPhoneContainer);
-  orderPhoneContainer.appendChild(orderPhoneIcon);
+  orderPhoneContainer.appendChild(orderPhoneLink);
+  orderPhoneLink.appendChild(orderPhoneIcon);
   orderPhoneContainer.appendChild(orderPhoneText);
 
   contactContainer.appendChild(dylanMailContainer);
-  dylanMailContainer.appendChild(dylanMailIcon);
+  dylanMailContainer.appendChild(dylanMailLink);
+  dylanMailLink.appendChild(dylanMailIcon);
   dylanMailContainer.appendChild(dylanMailText);
 
   contactContainer.appendChild(arloMailContainer);
-  arloMailContainer.appendChild(arloMailIcon);
+  arloMailContainer.appendChild(arloMailLink);
+  arloMailLink.appendChild(arloMailIcon);
   arloMailContainer.appendChild(arloMailText);
 
   contactContainer.appendChild(officePhoneContainer);
-  officePhoneContainer.appendChild(officePhoneIcon);
+  officePhoneContainer.appendChild(officePhoneLink);
+  officePhoneLink.appendChild(officePhoneIcon);
   officePhoneContainer.appendChild(officePhoneText);
 }
