a3b32e4940c7baf2e84804f213e4430f61c153e3
Dylan Premo <cyhyraethz@gmail.com>
2021-12-29 17:01:27 -0800

    Add screen reader only text to icons

--- src/contact-tab.js
+++ src/contact-tab.js
@@ -12,21 +12,25 @@ export default function loadContactTab() {
   const orderPhoneContainer = document.createElement('div');
   const orderPhoneLink = document.createElement('a');
   const orderPhoneIcon = document.createElement('i');
+  const orderPhoneIconText = document.createElement('Span');
   const orderPhoneText = document.createElement('p');
 
   const dylanMailContainer = document.createElement('div');
   const dylanMailLink = document.createElement('a');
   const dylanMailIcon = document.createElement('i');
+  const dylanMailIconText = document.createElement('Span');
   const dylanMailText = document.createElement('p');
 
   const arloMailContainer = document.createElement('div');
   const arloMailLink = document.createElement('a');
   const arloMailIcon = document.createElement('i');
+  const arloMailIconText = document.createElement('Span');
   const arloMailText = document.createElement('p');
 
   const officePhoneContainer = document.createElement('div');
   const officePhoneLink = document.createElement('a');
   const officePhoneIcon = document.createElement('i');
+  const officePhoneIconText = document.createElement('Span');
   const officePhoneText = document.createElement('p');
 
   contactName.id = 'contactName';
@@ -39,24 +43,32 @@ export default function loadContactTab() {
   orderPhoneLink.href = 'tel:8315550189';
   orderPhoneIcon.className = 'fas fa-phone-alt';
   orderPhoneIcon.setAttribute('aria-hidden', 'true');
+  orderPhoneIconText.className = 'sr-only';
+  orderPhoneIconText.innerText = 'Call to Place an Order';
   orderPhoneText.innerText = 'Call to Place an Order: 831.555.0189';
 
   dylanMailContainer.className = 'contactItemContainer';
   dylanMailLink.href = 'mailto:dylan@thegreensproutrestaurant.com';
   dylanMailIcon.className = 'fas fa-envelope';
   dylanMailIcon.setAttribute('aria-hidden', 'true');
+  dylanMailIconText.className = 'sr-only';
+  dylanMailIconText.innerText = 'dylan@thegreensproutrestaurant.com';
   dylanMailText.innerText = 'dylan@thegreensproutrestaurant.com';
 
   arloMailContainer.className = 'contactItemContainer';
   arloMailLink.href = 'mailto:arlo@thegreensproutrestaurant.com';
   arloMailIcon.className = 'fas fa-envelope';
   arloMailIcon.setAttribute('aria-hidden', 'true');
+  arloMailIconText.className = 'sr-only';
+  arloMailIconText.innerText = 'arlo@thegreensproutrestaurant.com';
   arloMailText.innerText = 'arlo@thegreensproutrestaurant.com';
 
   officePhoneContainer.className = 'contactItemContainer';
   officePhoneLink.href = 'tel:8315550190';
   officePhoneIcon.className = 'fas fa-phone-alt';
   officePhoneIcon.setAttribute('aria-hidden', 'true');
+  officePhoneIconText.className = 'sr-only';
+  officePhoneIconText.innerText = 'Office Number';
   officePhoneText.innerText = 'Office Number: 831.555.0190';
 
   content.appendChild(contactName);
